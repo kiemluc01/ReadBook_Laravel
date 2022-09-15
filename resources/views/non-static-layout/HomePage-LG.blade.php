aea2cb8273cb6091671189cf37f7fb2228d7acbf
kiemluc01 <kiemluc01@gmail.com>
2022-09-15 14:38:28 +0700

    show book

--- resources/views/non-static-layout/HomePage-LG.blade.php
+++ resources/views/non-static-layout/HomePage-LG.blade.php
@@ -16,30 +16,37 @@
         $cats = App\Http\Controllers\BookController::getCat();
     @endphp
     @foreach($cats as $cat)
-    <div class="row">
-        <h1>{{ $cat->Tendanhmuc }}</h1>
-        <div class="item-row">
         @php
             $books = App\Http\Controllers\BookController::getBookCat($cat->idDanhmuc);
-            $i = 1;
-        @endphp 
-            @foreach($books as $book)
-                <a href="/Book?id=1" class="item">
-                        <div class="img">
-                            <img src="{{ $book->imgSach; }}" alt="ảnh">
-                        </div>
-                        <h5>{{ $book->Tensach; }}</h5>
-                        <h6> {{ $book->Tacgia; }} </h5>
-                        <center><p>{{ $book->TomtatND; }}</p></center>
-                </a>
-                @php $i++; @endphp
-                @if($i == 6)
-        </div>
-        <div class="item-row">
-                @endif
-            @endforeach
-        </div>
-    </div>
+        @endphp
+        @foreach($books as $bookcheck)
+            @if($bookcheck->Tensach != NULL)
+                <div class="row">
+                    <h1>{{ $cat->Tendanhmuc }}</h1>
+                    <div class="item-row">
+                    @php
+                        $i = 1;
+                    @endphp 
+                        @foreach($books as $book)
+                            <a href="/Book?id=1" class="item">
+                                    <div class="img">
+                                        <img src="{{ $book->imgSach; }}" alt="ảnh">
+                                    </div>
+                                    <h5>{{ $book->Tensach; }}</h5>
+                                    <h6> {{ $book->Tacgia; }} </h5>
+                                    <center><p>{{ $book->TomtatND; }}</p></center>
+                            </a>
+                            @php $i++; @endphp
+                            @if($i > 5)
+                    </div>
+                    <div class="item-row">
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+            @break;
+        @endforeach
     @endforeach
     <!-- end row  -->
 </div>
